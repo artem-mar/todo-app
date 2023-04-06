f1a1154d08544fc02ed1d255c56adde6ff5eb408
Artem M <artemmarin91@mail.ru>
2023-04-06 12:01:39 +0300

    added e2e tests

---
+++ tests/myTest.spec.js
@@ -0,0 +1,56 @@
+const { test, expect } = require('@playwright/test');
+
+// test('open form', async ({ page }) => {
+//   await page.goto('/');
+
+//   // Expect a title "to contain" a substring.
+//   await page.getByRole('button').first().click();
+
+//   // форма открылась
+//   await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
+
+//   // далее закрываем форму
+//   await page.getByRole('button', { name: 'Отмена' }).click();
+
+//   // снова проверяем, тут тесты падают
+//   await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
+// });
+
+// test('add a new task', async ({ page }) => {
+//   await page.goto('/');
+
+//   // Expect a title "to contain" a substring.
+//   await page.getByRole('button').first().click();
+
+//   await page.getByPlaceholder('Название задачи').fill('test task');
+//   await page.getByPlaceholder('Введите описание').fill('description');
+//   await page.getByRole('textbox', { name: 'Дедлайн' }).fill('2023-04-10T10:20');
+
+//   await page.getByRole('button', { name: 'Отправить' }).click();
+
+//   await expect(page.getByText('test task')).toBeVisible();
+//   await expect(page.getByText('10 апреля')).toBeVisible();
+//   await expect(page.getByText('описание')).toBeVisible();
+
+//   await page.getByTestId('delete test task').click();
+
+//   await expect(page.getByText('10 апреля')).toBeVisible();
+// });
+
+test('edit task', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await page.getByTestId('edit task').first().click();
+
+  await expect(page.locator('_react=Form')).toBeVisible();
+});
+
+// test('delete task', async ({ page }) => {
+//   await page.goto('/');
+
+//   await page.getByTestId('delete test task').click();
+
+//   // тут по идее тесты должны падать
+//   await expect(page.getByText('10 апреля')).toBeVisible();
+// });
