549ff70636e66194656e7875a6896665cec9dd36
Artem M <artemmarin91@mail.ru>
2023-04-06 12:08:06 +0300

    uncoment

--- tests/myTest.spec.js
+++ tests/myTest.spec.js
@@ -1,41 +1,37 @@
 const { test, expect } = require('@playwright/test');
 
-// test('open form', async ({ page }) => {
-//   await page.goto('/');
+test('open form', async ({ page }) => {
+  await page.goto('/');
 
-//   // Expect a title "to contain" a substring.
-//   await page.getByRole('button').first().click();
+  // Expect a title "to contain" a substring.
+  await page.getByRole('button').first().click();
 
-//   // форма открылась
-//   await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
+  // форма открылась
+  await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
 
-//   // далее закрываем форму
-//   await page.getByRole('button', { name: 'Отмена' }).click();
+  // далее закрываем форму
+  await page.getByRole('button', { name: 'Отмена' }).click();
 
-//   // снова проверяем, тут тесты падают
-//   await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
-// });
+  // снова проверяем, тут тесты падают
+  await expect(page.getByPlaceholder('Название задачи')).toBeVisible();
+});
 
-// test('add a new task', async ({ page }) => {
-//   await page.goto('/');
+test('add a new task', async ({ page }) => {
+  await page.goto('/');
 
-//   // Expect a title "to contain" a substring.
-//   await page.getByRole('button').first().click();
+  // Expect a title "to contain" a substring.
+  await page.getByRole('button').first().click();
 
-//   await page.getByPlaceholder('Название задачи').fill('test task');
-//   await page.getByPlaceholder('Введите описание').fill('description');
-//   await page.getByRole('textbox', { name: 'Дедлайн' }).fill('2023-04-10T10:20');
+  await page.getByPlaceholder('Название задачи').fill('test task');
+  await page.getByPlaceholder('Введите описание').fill('description');
+  await page.getByRole('textbox', { name: 'Дедлайн' }).fill('2023-04-10T10:20');
 
-//   await page.getByRole('button', { name: 'Отправить' }).click();
+  await page.getByRole('button', { name: 'Отправить' }).click();
 
-//   await expect(page.getByText('test task')).toBeVisible();
-//   await expect(page.getByText('10 апреля')).toBeVisible();
-//   await expect(page.getByText('описание')).toBeVisible();
-
-//   await page.getByTestId('delete test task').click();
-
-//   await expect(page.getByText('10 апреля')).toBeVisible();
-// });
+  await expect(page.getByText('test task')).toBeVisible();
+  await expect(page.getByText('10 апреля')).toBeVisible();
+  await expect(page.getByText('описание')).toBeVisible();
+});
 
 test('edit task', async ({ page }) => {
   await page.goto('/');
